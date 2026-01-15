2358d3b5ff4fd963849d69d381267e136fce8a45
Lidia Tursunova <116261802+ScooLida@users.noreply.github.com>
2026-01-15 13:34:33 +0300

    Create work_script.sh

---
+++ work_script.sh
@@ -0,0 +1,23 @@
+#проверка на наличие гриба
+#!/bin/bash
+
+for file in ~/Numenius/*_R1.fastq;
+ do
+  name=$(basename "${file%_R1.fastq}")  
+
+  echo "Обработка файла: $file" | tee -a otchet.txt
+
+   ~/kraken2-2.1.3/bin/kraken2 --db my_fungi_db \
+     --threads 10 --paired \
+     --report "${name}_rep.txt" \
+     --output "${name}_out.txt" \
+   ~/Numenius/"${name}_R1.fastq" ~/Numenius/"${name}_R2.fastq" 
+  done > otchet.txt
+touch all_rep.txt
+> all_rep.txt
+ for  rep in *_rep.txt; do
+  Sname=$(basename "${rep%_rep.txt}")
+  if grep  -q "153531" "$rep"; then echo "$Sname" >> all_rep.txt
+ fi
+done
+
